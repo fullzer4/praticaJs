21a2e0b8c7f81cb6a48d02219d96b840e8db50e9
fullzer4 <53914594+fullzer4@users.noreply.github.com>
2022-05-09 23:12:15 -0300

    att

--- loops/while.js
+++ loops/while.js
@@ -1,7 +1,6 @@
 x = 0;
 
-while(x < 10){ //enquanto x for menor que 10
-    console.log("O valor do x e:" + x)
-
-    x++; //somar 1
+while(x < 12){
+    console.log("eu sou o gabriel");
+    x++;
 }
